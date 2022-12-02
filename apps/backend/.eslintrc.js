37b621e6bd991f08aa77119c3f892eae467189b4
Timur <timurtokaev.toombez@gmail.com>
2022-12-03 01:35:29 +0300

    Init nest app

---
+++ apps/backend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@realine/eslint-config-custom'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+}
