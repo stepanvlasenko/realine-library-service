a5a6130afb663500bd27420b329cc399e3d531a2
stepanvlasenko <reyden732@gmail.com>
2022-12-21 23:06:56 +0300

    create book component add typed props variant and book

---
+++ apps/web/components/book.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+import { Variant, IBook } from '@types'
+
+defineProps({
+    variant: {
+        type: String as PropType<Variant>,
+        required: true
+    },
+    book: {
+        type: Object as PropType<IBook>,
+        required: true
+    }
+})
+</script>
