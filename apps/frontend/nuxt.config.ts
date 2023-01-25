cbcf925d1e911293d36d2540421651f54ed131bb
Timur <timurtokaev.toombez@gmail.com>
2023-01-26 01:56:32 +0300

    Register colors

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,9 +1,14 @@
 import path from 'path'
 
 export default defineNuxtConfig({
+    css: [
+        '@/assets/styles/index.scss',
+    ],
+
     nitro: {
         preset: 'netlify',
     },
+
     alias: {
         '@types': path.join(__dirname, 'assets', 'ts', 'types.ts'),
     },
