95246fb466d06cb55d9b20e6c2e5a259dc92b04f
Timur <timurtokaev.toombez@gmail.com>
2023-01-26 02:43:04 +0300

    Clear app

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { useScssVariables } from '@/compasables/useScssBreakpoints'
-
-console.log(useScssVariables().desktop)
-</script>
-
 <template>
     <div>
         Lorem, ipsum.
