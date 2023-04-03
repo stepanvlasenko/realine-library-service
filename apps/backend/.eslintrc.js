26fc90817c32049b0255905ab73b8cb821ce6a51
Timur <81884993+toombez@users.noreply.github.com>
2023-04-03 22:02:23 +0300

    Feat/remove-turborepo (#9)

--- apps/backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['@realine/eslint-config-custom'],
-    rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-    },
-}
