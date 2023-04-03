99acd886a60198f921d32b8a12d0e99b9990e734
Timur <timurtokaev.toombez@gmail.com>
2023-04-03 21:54:18 +0300

    Remove turborepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
     root: true,
-    extends: ['@realine/eslint-config-custom'],
-    settings: {
-        next: {
-            rootDir: ['apps/*/'],
-        },
+    extends: ['@antfu'],
+    rules: {
+        'indent': ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+        'vue/html-indent': ['error', 4],
     },
 }
